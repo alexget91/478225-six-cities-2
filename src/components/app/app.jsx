c279b0d5d64ad3dfa8f2a99ab2e3c49a86fce3fe
Aleksey Getmanets <alex.get91@gmail.com>
2019-10-19 15:15:04 +0300

    Отображение списка объектов недвижимости в соответствии со списком названий объектов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Index from "../index";
+import PropTypes from 'prop-types';
 
-const App = () => {
-  return <Index/>;
+const App = (props) => {
+  const {placeNames} = props;
+
+  return <Index placeNames={placeNames}/>;
+};
+
+App.propTypes = {
+  placeNames: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default App;
