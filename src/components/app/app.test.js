1b9ae95aaffbe95dbdf3d12666bc5f1240ea80f1
Aleksey Getmanets <alex.get91@gmail.com>
2019-11-01 18:43:42 +0300

    Добавлен компонент «Карта»

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,14 +1,21 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import App from "./app";
+import {createMapBlock} from "../../common/test-stubs";
 
 it(`App correctly renders after relaunch`, () => {
+  createMapBlock();
+
   const tree = renderer
     .create(<App offers={[{
       id: 0,
       priceByNight: 0,
       title: ``,
       type: `apartment`,
+      location: {
+        latitude: 0,
+        longitude: 0,
+      }
     }]}/>)
     .toJSON();
 
